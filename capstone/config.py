8c4ab215082851c753535188aa2a8d43114df13e
PShubhank2004 <shubhank856914@gmail.com>
2026-01-31 22:12:22 +0530

    Update config.py

--- capstone/config.py
+++ capstone/config.py
@@ -1,11 +1,12 @@
 # config.py
 
-MONGO_URI = "mongodb+srv://capstone_shubhank:shubhank@capstonecluster.p3dpndo.mongodb.net/?appName=CapstoneCluster"
+MONGO_URI = ""
 DB_NAME = "capstone_db"
 
 BOOKS_COLLECTION = "books"
 SERVICES_COLLECTION = "developer_services"
 USER_ACTIVITY_COLLECTION = "user_activity"
 USERS_COLLECTION = "users"
-GEMINI_API_KEY = "AIzaSyBQqnEqGvHZcdCAoI1R6m3I7C4OHiuGaGE"
+GEMINI_API_KEY = ""
 GEMINI_MODEL = "gemini-2.5-flash"
+
